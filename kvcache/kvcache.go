3e78c8775ba88cfdb7ac85191d413987e62fde07
Suzanne Richman <srichm@ext.uber.com>
2019-02-25 16:23:17 -0800

    still working with the put method and the cache to figureout WHY I cannot get values in the test. Code now matches Scott's exactly as far as I can tell. Troy will have time to review in the morning.

--- kvcache/kvcache.go
+++ kvcache/kvcache.go
@@ -16,18 +16,20 @@ type SimpleKeyValueCache struct{
 	data map[string]string
 }
 
-//per discussion, thought about order of operations - needs to have a cache for the method to work
+//AHA! I was missing the constructor function for the cache! This is why tests were breaking (I hope)
+func NewSimpleKVCache() *SimpleKeyValueCache{
+	return &SimpleKeyValueCache{map[string]string{}}
+}
+
+//per Troy don't need to check for cache here, this is a method of c - it is like "this in Java"
 func (c *SimpleKeyValueCache) Put(key,value string) error{
-	if !(c == nil) {
-		if !(key == "" && value == "") {
+
 			_, keyExists := c.data[key]
 			if keyExists {
 				c.data[key] = value
 				return nil
 			}
-		}
-	}
-		return fmt.Errorf("put failed: cache '%v' does not exists", c)
+		return fmt.Errorf("put failed: key '%v' or value '%v' does not exist: ",key, value)
 }
 
 func (c *SimpleKeyValueCache) Read(key string) string{
@@ -50,10 +52,4 @@ func (c *SimpleKeyValueCache) Delete(key string) error{
 		return nil
 	}
 	return fmt.Errorf("delete failed: key '%v' not in cache",key)
-}
-
-type toFileKeyValueCache struct{
-	//members (to append reads to a file) - to check output
-	//run till we get an error or check output
-	//cache *SimpleKeyValueCache - what ever it reads return it to file
 }
